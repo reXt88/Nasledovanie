fa5205c19efaec88a2ab07ee068a4b79acafb9fb
h <g>
2022-03-28 13:30:16 +0300

    Добавьте файлы проекта.

---
+++ Animal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasledovanie
+{
+    public class Animal
+    {
+        public static string Name;
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public new void Show()
+        {
+            Console.WriteLine(Name);
+        }
+        public new void Voice()
+        {
+            Console.WriteLine("Мычание");
+        }
+    }
+    public class Cat : Animal
+    {
+        string Color;
+        public Cat(string Name, string color) : base(Name)
+        {
+            Color = color;
+        }
+        public new void Show()
+        {
+            Console.WriteLine(Name + " " + Color);
+        }
+        public new void Voice()
+        {
+            Console.WriteLine("Мяу-мяу");
+        }
+    }
+    public class Dog : Animal
+    {
+        string Color;
+        public Dog(string Name, string color) : base(Name)
+        {
+            Color = color;
+        }
+        public new void Show()
+        {
+            Console.WriteLine(Name + " " + Color);
+        }
+        public new void Voice()
+        {
+            Console.WriteLine("Гав-гав");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasledovanie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog qwe = new Dog("Джек", "коричневый");
+            qwe.Show();
+            qwe.Voice();
+            Cat asd = new Cat("Мурка", "серая");
+            asd.Show();
+            asd.Voice();
+            Console.ReadKey();
+        }
+    }
+}
